c4bbf614db231bc7a3c86b6bfaab37909bcf6402
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-12 01:06:57 +0700

    adding a little changes

--- costumer/admin.py
+++ costumer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from costumer.models import costumer
+
 # Register your models here.
+
+class admin_costumer(admin.ModelAdmin):
+
+	list_display = ['kd_costumer','nama_costumer','alamat_costumer']
+	search_fields = ['kd_costumer','nama_costumer']
+	list_per_page = 20
+
+admin.site.register(costumer, admin_costumer)
