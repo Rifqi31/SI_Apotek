887b2fea875224e1763ad3ed99155a8d8b6fc1fe
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-24 15:39:53 +0700

    add admin resep, and modified url and homepage

--- Resep/admin.py
+++ Resep/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from Resep.models import*
+
 # Register your models here.
+
+class Admin_Resep(admin.ModelAdmin):
+
+	list_display = ['kode_resep','tanggal_resep','nama_pasien']
+	search_fields = ['kode_resep','nama_pasien']
+	list_per_page = 15
+
+admin.site_register(Resep, Admin_Resep)
