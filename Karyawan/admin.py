de68f211f14f93aab00be597e1b5c663c2681a5a
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-24 10:09:02 +0700

    add karyawan admin page

--- Karyawan/admin.py
+++ Karyawan/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from Karyawan.models import*
 # Register your models here.
+
+class Admin_Karyawan(admin.ModelAdmin):
+
+    list_display = ['kode_karyawan','nama_karyawan','tanggal_lahir_karyawan','alamat_karyawawn','telepon_karyawan','email_karyawan']
+    search_fields = ['nama_karyawan','kode_karyawan','telepon_karyawan','email_karyawan']
+    list_per_page = 15
+
+admin.site.register(Karyawan, Admin_Karyawan)
