98f4c028661e50764766ad2dc260b01e1ffca9c7
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-24 14:14:27 +0700

    modified admin pemesanan

--- Pemesanan/admin.py
+++ Pemesanan/admin.py
@@ -10,3 +10,12 @@ class Admin_Pemesanan(admin.ModelAdmin):
     list_per_page = 15
 
 admin.site.register(Pemesanan, Admin_Pemesanan)
+
+
+class Admin_DetailPemesanan(admin.ModelAdmin):
+
+    list_display = ['kode_pemesanan','kode_obat','kode_resep','jumlah']
+    search_fields = ['kode_pemesanan','kode_obat','kode_resep']
+    list_per_page = 15
+
+admin.site.register(DetailPemesanan, Admin_DetailPemesanan)
