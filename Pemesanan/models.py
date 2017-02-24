27b7240c3e2748f5b706e4ea8bc9671490503717
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-24 09:07:33 +0700

    add models of pemesanan app

--- Pemesanan/models.py
+++ Pemesanan/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from django.core.validators import RegexValidator
+import datetime
+
+#import models
+from Pelanggan.models import*
 
 # Create your models here.
+
+class Pemesanan(models.Model):
+
+    kode_pemesanan = models.CharField(
+        primary_key = True,
+        max_length = 5,
+        validators=[RegexValidator(r'^\d{1,10}$')])
+    )
+
+    tanggal_pemesanan = models.DateField(auto_now_add = True)
+    kode_pelanggan = models.ForeignKey(Pelanggan)
+    kode_karyawan = models.ForeignKey(Karyawan)
+
+    def __unicode__(self):
+        return self.kode_pemesanan
