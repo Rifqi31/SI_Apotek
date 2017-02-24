b3c49ba0d1b53b0cd0a3a682276bdc109c768b28
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-24 09:12:10 +0700

    add admin page

--- Pemesanan/admin.py
+++ Pemesanan/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from Pemesanan.models import*
 # Register your models here.
+
+class Admin_Pemesanan(admin.ModelAdmin):
+
+    list_display = ['kode_pemesanan','tanggal_pemesanan','kode_pelanggan','kode_karyawan']
+    search_fields = ['kode_pemesanan','kode_karyawan']
+    list_per_page = 15
+
+admin.site.register(Pemesanan, Admin_Pemesanan)
