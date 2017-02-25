5a34410071d0bfa97c17b22679c604a3f890feb9
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-25 11:32:53 +0700

    modified suplier admin

--- Suplier/admin.py
+++ Suplier/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from Suplier.models import*
+
 # Register your models here.
+
+class Admin_Suplier(admin.ModelAdmin):
+
+	list_display = ['kode_suplier','nama_suplier','alamat_suplier','telepon_suplier','email_suplier']
+	search_fields = ['kode_suplier','nama_suplier','telepon_suplier','email_suplier']
+	list_per_page = 15
+
+admin.site.register(Suplier, Admin_Suplier)
