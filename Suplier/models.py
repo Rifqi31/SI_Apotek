8712d40022c770649720433b8b30cc512157b45c
rifqi31 <muttaqinrifqi31@gmail.com>
2017-02-25 11:27:47 +0700

    add suplier models

--- Suplier/models.py
+++ Suplier/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from django.core.validators import RegexValidator
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
+
+class Suplier(models.Model):
+
+	kode_suplier = models.CharField(
+		primary_key = True,
+		max_length = 5,
+		validators=[RegexValidator(r'^\d{1,5}$')]
+	)
+
+	nama_suplier = models.CharField(max_length = 50)
+	alamat_suplier = models.TextField(validators=[MaxLengthValidator(100)])
+	telepon_suplier = models.CharField(max_length = 20, validators=[RegexValidator(r'^\d{1,12}$')])
+	email_suplier = models.EmailField()
+
+	def __unicode__(self):
+		return self.kode_suplier
